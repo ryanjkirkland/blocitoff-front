c350b48eb625d9ffed7a91246641be032fa935e6
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2017-01-27 17:31:15 -0800

    Hide tasks older than 7 days using ngHide

--- app/scripts/controllers/TaskCtrl.js
+++ app/scripts/controllers/TaskCtrl.js
@@ -2,12 +2,33 @@
   function TaskCtrl($scope, $firebaseArray) {
     var ref = firebase.database().ref().child('tasks');
     $scope.tasks = $firebaseArray(ref);
-
-    $scope.createTask = function() {
-      $scope.tasks.add$({
-        name: $scope.newTask,
-        created_at: Time.now
+    window.foo = $scope.tasks;
+    $scope.addNewTask = function() {
+      $scope.tasks.$add({
+        name: $scope.newTask.name,
+        createdAt: Date.now()
       });
+
+      $scope.newTask = {}
+    };
+
+    $scope.oldTask = function(task) {
+      var today = Date.now();
+
+      if ((today - task.createdAt) < 604800000) {
+        return false;
+      }  else {
+        return true;
+      }
+
+      /*for(i = 0; i <= tasks.length; i++) {
+        if ((today - tasks[i].createdAt) < 604800000) {
+          console.log(false);
+        }  else {
+          console.log(tasks[i]);
+          console.log(false);
+        }
+      }*/
     };
   }
 
